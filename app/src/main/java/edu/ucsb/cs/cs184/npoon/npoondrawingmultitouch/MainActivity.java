4a32724823e686bfe37362288e52a83f6f16b1cf
nickkpoon <nickkpoon@gmail.com>
2017-10-23 14:29:47 -0700

    TOUCH ON CONSTRAINT LAYOUT WORKING

--- app/src/main/java/edu/ucsb/cs/cs184/npoon/npoondrawingmultitouch/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs184/npoon/npoondrawingmultitouch/MainActivity.java
@@ -2,12 +2,15 @@ package edu.ucsb.cs.cs184.npoon.npoondrawingmultitouch;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
@@ -37,8 +40,25 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Bitmap b = Bitmap.createBitmap(100,100,Bitmap.Config.ARGB_8888);
-        Canvas c = new Canvas (b);
+        ConstraintLayout myLayout = (ConstraintLayout) findViewById(R.id.myLayout);
 
+        //Bitmap b = Bitmap.createBitmap(100,100,Bitmap.Config.ARGB_8888);
+        //Canvas c = new Canvas (b);
+
+        myLayout.setOnTouchListener(new ConstraintLayout.OnTouchListener(){
+            public boolean onTouch(View v, MotionEvent m)
+            {
+                handleTouch();
+                return true;
+            }
+
+        });
     }
+
+    public void handleTouch()
+    {
+        Toast.makeText(MainActivity.this, "multiTouch clicked", Toast.LENGTH_LONG).show();
+    }
+
+
 }
